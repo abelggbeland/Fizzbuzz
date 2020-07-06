e7c781e2fd262d8282a3419ae7d657a914cdc806
Abel Beland <abelbeland@gmail.com>
2020-07-06 16:46:38 -0600

    added one line

---
+++ FizzBuzzOneLine/FizzBuzzOneLine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzzOneLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{((i % 3 == 0) ? "Fizz " : "")}" +
+                                  $"{((i % 5 == 0) ? "Bizz " : "")}" +
+                                  $"{((!(i % 5 == 0) && !(i % 3 == 0)) ? i.ToString() : "")}");
+            }
+        }
+    }
+}
